f4a8acc3f4915218ec6ed570f7aa019d32d0c60d
mloisotto <mloisotto@gmail.com>
2018-04-23 18:15:22 -0300

    Returns data in a JSON instead of a json string.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-'use strict';
-
 import { NativeModules } from 'react-native';
 
 const { SmartLockRN } = NativeModules;
 
-export default SmartLockRN;
+const smartLock = {};
+
+smartLock.getCredentials = () => {
+  return new Promise((resolve, reject) => {
+    SmartLockRN.getCredentials()
+      .then(data => resolve(JSON.parse(data)))
+      .catch(err => reject(err));
+  });
+}
+
+
+export default smartLock;
